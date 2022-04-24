f2158ef6dfc690a8ab27c0d21f128acf7276ffb7
ابن الحي <75506961+abnhay@users.noreply.github.com>
2022-04-24 20:20:07 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update -y
+pkg upgrade -y
+apt update && apt upgrade -y
+pkg install python -y
+pip install socket
+pip install requests
+pip install pyfiglet
+pip install getmac
+pip install webbrowser
+clear
+python Ip-Address.py
